ffa63dd27fc3e780907e2665b538ade7f40a5855
Chandramouli Subrahmanyam Guna <mouli.guna24@gmail.com>
2021-03-29 17:37:49 +0100

    Add files via upload

--- test2.py
+++ test2.py
@@ -54,8 +54,36 @@ validation_dataset = image_dataset_from_directory("./validation/",
 class_names = train_dataset.class_names
 
 #below code is used to train mobileNet model and save it as model.h5
-# model.fit(train_dataset, epochs=25,validation_data=validation_dataset)
+# history = model.fit(train_dataset, epochs=25,validation_data=validation_dataset)
 # model.save('model.h5')
+
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+#
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([min(plt.ylim()),1])
+# plt.title('Training and Validation Accuracy')
+#
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.ylabel('Cross Entropy')
+# plt.ylim([0,1.0])
+# plt.title('Training and Validation Loss')
+# plt.xlabel('epoch')
+# plt.show()
+
+
+
 model = keras.models.load_model('model.h5')
 
 
